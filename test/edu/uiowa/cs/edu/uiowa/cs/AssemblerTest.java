185266a23c3ec5834e0a1a766c1535b1b6d388f4
nboomgarden <nikki-boomgarden@uiowa.edu>
2017-09-19 17:13:52 -0500

    updating test2 - feel free to look and review, especially part 2's portion of test 2

--- test/edu/uiowa/cs/edu/uiowa/cs/AssemblerTest.java
+++ test/edu/uiowa/cs/edu/uiowa/cs/AssemblerTest.java
@@ -10,20 +10,20 @@ import static org.junit.Assert.*;
 public class AssemblerTest {
         private static int MARS_TEXT_SEGMENT_START = 0x00400000;
 
-        private static void testHelper(List<Instruction> input, Instruction[] expectedP1/*, Instruction[] expectedP2, Integer[] expectedP3*/) {
+        private static void testHelper(List<Instruction> input, Instruction[] expectedP1, Instruction[] expectedP2, Integer[] expectedP3) {
             // Phase 1
             List<Instruction> tals = Phase1.mal_to_tal(input);
             System.out.print(tals.toString());
             assertArrayEquals(expectedP1, tals.toArray());
             
-            /*
+            
             // Phase 2
             List<Instruction> resolved_tals = Phase2.resolve_addresses(tals, MARS_TEXT_SEGMENT_START);
             assertArrayEquals(expectedP2, resolved_tals.toArray());
 
             // Phase 3
-            List<Integer> translated = Phase3.translate_instructions(resolved_tals);
-            assertArrayEquals(expectedP3, translated.toArray());*/
+            List<Integer> translated = Phase3.translate_instructions(resolved_tals);        
+            assertArrayEquals(expectedP3, translated.toArray());
         }
 
         @Test
@@ -51,7 +51,7 @@ public class AssemblerTest {
             };
 
             // Phase 2
-            /*Instruction[] phase2_expected = {
+            Instruction[] phase2_expected = {
                     InstructionFactory.CreateAddu(8,0, 0, "label1"),//new Instruction(2,8,0,0,0,0,0,1,0),
                     InstructionFactory.CreateAddu(16,23,12),//new Instruction(2,16,23,12,0,0,0,0,0),
                     InstructionFactory.CreateSlt(1, 16, 8),//new Instruction(8,1,16,8,0,0,0,0,0),
@@ -71,19 +71,79 @@ public class AssemblerTest {
                     0x3c0100f0,
                     0x34210000,
                     0x02418821
-            };*/
+            };
 
 
             testHelper(input,
-                    phase1_expected);/*,
                     phase1_expected,
-                    phase1_expected);*/
+                    phase2_expected,
+                    phase3_expected);
         }
+        private static void test2Helper(List<Instruction> input2, Instruction[] test2expectedP1, Instruction[] test2expectedP2, Integer[] test2expectedP3) {
+        // Phase 1
+        List<Instruction> tals2 = Phase1.mal_to_tal(input2);
+        System.out.print(tals2.toString());
+        assertArrayEquals(test2expectedP1, tals2.toArray());
+            
+            
+        // Phase 2
+        List<Instruction> resolved_tals2 = Phase2.resolve_addresses(tals2, MARS_TEXT_SEGMENT_START);
+        assertArrayEquals(test2expectedP2, resolved_tals2.toArray());
 
+        // Phase 3
+        List<Integer> translated2 = Phase3.translate_instructions(resolved_tals2);        
+        assertArrayEquals(test2expectedP3, translated2.toArray());
+        }
         @Test
         public void test2() {
             /* Fill in your additional test case here! */
 
             //testHelper(...);
+
+            // test 2
+            List<Instruction> input2 = new LinkedList<Instruction>();
+            // or $k0, $s0, $t2
+            input2.add(InstructionFactory.CreateOr(16,10,26));
+            // beq $s0, $t5, hop
+            input2.add(InstructionFactory.CreateBeq(16, 13, "hop"));
+            // lui $t5, 0x0021
+            input2.add(InstructionFactory.CreateLui(13, 0x0021));
+            // bge $t2, $k0, hop 
+            input2.add(InstructionFactory.CreateBge(26, 16, "hop"));
+          
+            
+                        // Test 2, Phase 1
+            Instruction[] test2phase1_expected = {
+                    InstructionFactory.CreateOr(26, 10,16), //or $k0, $s0, $t2
+                    InstructionFactory.CreateBeq(16,13,"hop"), //beq $s0, $t5, hop
+                    InstructionFactory.CreateLui(13, 0x0021), //lui $t5, 0x0021
+                    InstructionFactory.CreateSlt(1, 26, 10, "hop"), //slt $at, $k0, $t2
+                    InstructionFactory.CreateBeq(1, 0, 0) //beq $at, $zero, $zero
+            };
+            
+                        // Test 2, Phase 2
+            Instruction[] test2phase2_expected = {
+                    InstructionFactory.CreateOr(26, 10,16), //new instruction(ID.or, rd, rs, rt, 0)
+                    InstructionFactory.CreateBeq(16,13,"hop"), //new instruction
+                    InstructionFactory.CreateLui(13, 0x0021), //new instruction
+                    InstructionFactory.CreateSlt(1, 26, 10, "hop"), //new instruction
+                    InstructionFactory.CreateBeq(1, 0, 0) //new instruction()
+            };
+            
+            
+            // Test 2 Phase 3
+            Integer[] test2phase3_expected = {
+                    // HINT: to get these, type the input program into MARS, assemble, and copy the binary values into your test case
+                0x020ad025,
+                0x120d0003,
+                0x3c0d0021,
+                0x015a082a,
+                0x10200000
+            };
+            
+            test2Helper(input2,
+                    test2phase1_expected,
+                    test2phase2_expected, //still need to complete phase 2 test 2
+                    test2phase3_expected);
         }
 }
