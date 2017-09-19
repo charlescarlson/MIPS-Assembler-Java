be5dd4aebeba6cf1010ae17674f265d52e84fb39
charlesc3929 <charles-carlson@uiowa.edu>
2017-09-19 12:01:53 -0500

    Phase 2: Loops are done. Now just have to do conversion from address to immediate

--- src/edu/uiowa/cs/Phase2.java
+++ src/edu/uiowa/cs/Phase2.java
@@ -17,25 +17,46 @@ public class Phase2 {
      * first_pc: address where the first instruction will eventually be placed in memory
      */
     
-    public static List<Instruction> addressResolvedTals = new LinkedList<Instruction>();
+    public static LinkedList<Instruction> addressResolvedTals = new LinkedList<Instruction>();
+    public static LinkedList<LinkedList<Object>> listOfMappings = new LinkedList<LinkedList<Object>>();
     
     public static List<Instruction> resolve_addresses(List<Instruction> unresolved, int first_pc) {
-        for (int i = 0; i < addressResolvedTals.size(); i++) {
-            Instruction currentInstruction = addressResolvedTals.get(i);
+        for (int i = 0; i < unresolved.size(); i++) {
+            Instruction currentInstruction = unresolved.get(i);
+            LinkedList<Object> mapping = new LinkedList<Object>();
             
             // if line has a label
             if (currentInstruction.label != "") {
                 // save the address of line
-            }
-            
-            // if instruction is a branch
-            if ( currentInstruction.instruction_id.blt == currentInstruction.instruction_id 
-                    || currentInstruction.instruction_id.bge == currentInstruction.instruction_id
-                    || currentInstruction.instruction_id.blt == currentInstruction.instruction_id
-                    || currentInstruction.instruction_id.beq == currentInstruction.instruction_id ) 
-            {
+                String label = currentInstruction.label;
+                int label_pc = first_pc;
+                mapping.add(label);
+                mapping.add(label_pc);
                 
             }
+            first_pc += 4;
+            if (mapping.size() != 0) {
+                listOfMappings.add(mapping);
+            }           
+            // if instruction is a branch
+            
+        }
+        
+        for (int i = 0; i < unresolved.size(); i++) {
+            Instruction currentInstruction = unresolved.get(i);
+            if (currentInstruction.instruction_id.beq == currentInstruction.instruction_id
+                    || currentInstruction.instruction_id.blt == currentInstruction.instruction_id
+                    || currentInstruction.instruction_id.bge == currentInstruction.instruction_id
+                    || currentInstruction.instruction_id.bne == currentInstruction.instruction_id ) {
+                for (int j = 0; j < listOfMappings.size(); j++) {
+                    // structure of elements in listOfMappings: {String label, int label_pc}
+                    LinkedList<Object> currentMapping = listOfMappings.get(j);
+                    if (currentMapping.get(0) == currentInstruction.branch_label) {
+                        currentInstruction.immediate = 22;
+                    }
+                }
+            }
+            addressResolvedTals.add(currentInstruction);
         }
         return addressResolvedTals;
     }
