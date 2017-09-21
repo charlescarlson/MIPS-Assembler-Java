2f91adf61acb609782b27eb9f21f02c7bce46be0
nboomgarden <nikki-boomgarden@uiowa.edu>
2017-09-20 20:24:30 -0500

    pushing test file - trying to figure out why it adds the both of them. Pushing before my computer dies.

--- test/edu/uiowa/cs/edu/uiowa/cs/AssemblerTest.java
+++ test/edu/uiowa/cs/edu/uiowa/cs/AssemblerTest.java
@@ -69,9 +69,9 @@ public class AssemblerTest {
                     0x00004021,
                     0x02ec8021,
                     0x0208082a,
-                    0x1420fffc,
-                    0x3c0100f0,
-                    0x34210000,
+                    0x1420FFFC,
+                    0x3c0100f0, 
+                    0x34210000, //errors here
                     0x02418821
             };
 
@@ -81,7 +81,24 @@ public class AssemblerTest {
                     phase2_expected,
                     phase3_expected);
         }
+        
+        private static void test2Helper(List<Instruction> input2, Instruction[] test2expectedP1, Instruction[] test2expectedP2, Integer[] test2expectedP3) {
+            // Phase 1
+            List<Instruction> tals2 = Phase1.mal_to_tal(input2);
+            System.out.print(tals2.toString());
+            assertArrayEquals(test2expectedP1, tals2.toArray());
+            
+            
+            // Phase 2
+            List<Instruction> resolved_tals2 = Phase2.resolve_addresses(tals2, MARS_TEXT_SEGMENT_START);
+            assertArrayEquals(test2expectedP2, resolved_tals2.toArray());
 
+            // Phase 3
+            List<Integer> translated2 = Phase3.translate_instructions(resolved_tals2);  
+            assertArrayEquals(test2expectedP3, translated2.toArray());
+            
+            
+        }
         @Test
         public void test2() {
             /* Fill in your additional test case here! */
@@ -129,9 +146,10 @@ public class AssemblerTest {
                 0x10200000
             };
             
-            testHelper(input2,
+            test2Helper(input2,
                     test2phase1_expected,
-                    test2phase2_expected, //still need to complete phase 2 test 2
+                    test2phase2_expected,
                     test2phase3_expected);
         }
+
 }
